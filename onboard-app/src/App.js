b7732aceb83ddec5f4c6cb62b7249f85cca0cb60
numbers0580 <koreankid18@yahoo.com>
2020-06-17 16:28:04 -0600

    Working on form functionality and validation.

--- onboard-app/src/App.js
+++ onboard-app/src/App.js
@@ -1,17 +1,81 @@
 import React, {useState, useEffect} from 'react';
 import Form from './Form';
 import axios from 'axios';
+import {v4} from 'uuid';
 import * as yup from 'yup';
 //import logo from './logo.svg';
 //import './App.css';
 
+const defaultValues = {uName: '', uEmail: '', uPass: '', tosAccept: false};
+const defaultErrors = {uName: '', uEmail: '', uPass: '', tosAccept: ''};
+
+const initUsers = [];
+const initDisabled = true;
+
 function App() {
+  const [users, setUsers] = useState(initUsers);
+  const [formEntries, updateEntries] = useState(defaultValues);
+  const [formErrors, updateErrors] = useState(defaultErrors);
+  const [isDisabled, changeDisabled] = useState(initDisabled);
+
+  const getUsers = function() {
+    axios.get()
+      .then(fetched => {
+        setUsers([...users, fetched.data]);
+      })
+      .catch(errorMsg => {
+        debugger
+        console.log('Error getting Users');
+      })
+      .finally(evt => {
+        updateEntries(defaultValues);
+      })
+  };
+
+  const clickedSubmit = function(submitEvent) {
+    submitEvent.preventDefault();
+
+    const newEntry = {
+      id: v4(),
+      uName: formEntries.uName.trim(),
+      uEmail: formEntries.uEmail.trim(),
+      uPass: formEntries.uPass.trim(),
+      tosAccept: formEntries.tosAccept
+    };
+  };
+
   return (
     <div className="App">
       <h2>Add New User</h2>
-      <Form />
+      <Form disability={isDisabled} formSubmit={clickedSubmit}/>
+      {/* <div className="tosDetails" id="tosToggle">
+        <h3>Terms of Service</h3>
+        <p> You agree to this standard boilerplate... blah blah blah... first-born child or equivalent... more technical jargon gibberish...
+        Our cookies will end up knowing things about you that you've forgotten, and they will be better parents to your kids than you... SkyNet is real (not even joking, 
+        look it up)... Resistance is Futile! All praise HAL 9000!</p>
+      </div> */}
     </div>
   );
+}
+
+function linkButton() {
+   let pageOpened = 0;
+
+   if(document.getElementById('tosToggle').style.display === 'block') {
+      pageOpened = 1;
+   } else {
+      pageOpened = 0;
+   }
+
+   if(pageOpened === 0) {
+      pageOpened = 2;
+   } else if(pageOpened === 1) {
+      pageOpened = 0;
+      document.getElementById("tosToggle").style.display = 'none';
+   } else {
+      pageOpened = 1;
+      document.getElementById("tosToggle").style.display = 'block';
+   }
 }
 
 export default App;
--- onboard-app/src/Form.js
+++ onboard-app/src/Form.js
@@ -1,25 +1,34 @@
 import React from 'react';
 
 function Form(props) {
+   const {disability, formSubmit} = props;
+
     return (
-        <form>
+        <form className="formulator" onSubmit={formSubmit}>
             <div className='formInput'>
-				<div><label>Name:</label></div>
-				<div><input type="text" /></div>
+               <div><label>Name:</label></div>
+               <div><input type="text" name="uName" maxLength="15" /></div>
             </div>
             <div className='formInput'>
-				<div><label>Email:</label></div>
-				<div><input type="email" /></div>
+               <div><label>Email:</label></div>
+               <div><input type="email" name="uEmail" /></div>
             </div>
             <div className='formInput'>
-				<div><label>Password:</label></div>
-				<div><input type="password" /></div>
+               <div><label>Password:</label></div>
+               <div><input type="password" name="uPass" /></div>
             </div>
             <div className='tosInput'>
-				<div><label>I agree to the <a href="">Terms of Service</a>:</label></div>
-				<div><input type="checkbox" /></div>
+               {/* <div><label>I agree to the <a onClick={linkButton} href="javascript:void(0)">Terms of Service</a>:</label></div> */}
+               <div><label>I agree to the <a href="https://www.google.com/search?q=terms+of+service&oq=terms+of+service&aqs=chrome..69i57.2527j0j1&sourceid=chrome&ie=UTF-8">Terms of Service</a>:</label></div>
+               <div><input type="checkbox" name="tosAccept" style={{width: '40px', height: '40px'}} /></div>
             </div>
-            <button>Submit</button>
+            <button type="submit" disabled={disability}>Submit</button>
+            <div className="tosDetails" id="tosToggle">
+               <h3>Terms of Service</h3>
+               <p> You agree to this standard boilerplate... blah blah blah... first-born child or equivalent... more technical jargon gibberish...
+               Our cookies will end up knowing things about you that you've forgotten, and they will be better parents to your kids than you... SkyNet is real (not even joking, 
+               look it up)... Resistance is Futile! All praise HAL 9000!</p>
+            </div>
         </form>
     );
 };
