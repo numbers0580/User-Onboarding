60d93229e0304a419824e05f487b46c3a8ae3876
numbers0580 <koreankid18@yahoo.com>
2020-06-18 17:09:46 -0600

    Completed Project. Found I had to interlace my validation testing with my input testing to make sure the validation tests didn't run before the POST from Submit button had time to finish.

--- onboard-app/cypress/integration/examples/form_test.js
+++ onboard-app/cypress/integration/examples/form_test.js
@@ -9,37 +9,56 @@ describe('Testing User Onboarding Form', () => {
         cy.visit('http://localhost:3000/')
     })
 
+    it('Checking form validation if name is too short', () => {
+        cy.get('input[name="uName"]').type('S')
+            .should('have.value', 'S')
+
+            cy.get('.errorMessages')
+                .contains('Username must be at least 4 characters')
+                .should('exist')
+    })
+
     it('Enters a name', () => {
-        cy.get('input[name="uName"]')
-            .type('Spiderman')
+        cy.get('input[name="uName"]').type('piderman')
             .should('have.value', 'Spiderman')
+    })
+
+    it('Checking form validation if email is incomplete', () => {
+        cy.get('input[name="uEmail"]').type('n')
+            .should('have.value', 'n')
+
+            cy.get('.errorMessages')
+                .contains('Please enter a valid email address')
+                .should('exist')
     })
 
     it('Enters an email', () => {
         cy.get('input[name="uEmail"]')
-            .type('notPeterParker@daily-bugle.com')
+            .type('otPeterParker@daily-bugle.com')
             .should('have.value', 'notPeterParker@daily-bugle.com')
+    })
+
+    it('Checking form validation if password is too short', () => {
+        cy.get('input[name="uPass"]').type('f')
+            .should('have.value', 'f')
+
+            cy.get('.errorMessages')
+                .contains('Passwords must be at least 8 characters long')
+                .should('exist')
     })
 
     it('Enters a password', () => {
         cy.get('input[name="uPass"]')
-            .type('friendlyWebcrawler!')
+            .type('riendlyWebcrawler!')
             .should('have.value', 'friendlyWebcrawler!')
     })
 
     it('Checks the ToS box', () => {
-        cy.get('input[name="tosAccept"]')
-            .check()
+        cy.get('input[name="tosAccept"]').check()
     })
 
     it('Clicks the Submit button', () => {
         //Went to https://docs.cypress.io/api/commands/click.html#Syntax to figure out this part
-        cy.contains('Submit')
-            .click()
-    })
-
-    it('Checking form validation if an input is empty', () => {
-        cy.get('input[name="uName"]')
-        .should('have.value', '')
+        cy.get('button').click()
     })
 })
